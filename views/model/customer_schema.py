9e1c63b71d9383b3a2529c8a4e7bacf38815850f
frankiennamdi <fejoh@sonatype.com>
2019-10-27 18:52:59 -0400

    IMPL: clean up documentation

--- views/model/customer_schema.py
+++ views/model/customer_schema.py
@@ -2,17 +2,17 @@ import graphene
 
 
 class UsageDetails(graphene.ObjectType):
-    apiCalls = graphene.String()
-    storage = graphene.String()
-    requestSize = graphene.String()
+    apiCalls = graphene.String(required=True)
+    storage = graphene.String(required=True)
+    requestSize = graphene.String(required=True)
 
 
 class UsageData:
-    subscriptionStartsAt = graphene.Int()
-    subscriptionEndsAt = graphene.Int()
-    status = graphene.String()
-    allowsApiOverage = graphene.Boolean()
-    details = graphene.Field(UsageDetails)
+    subscriptionStartsAt = graphene.Int(required=True)
+    subscriptionEndsAt = graphene.Int(required=True)
+    status = graphene.String(required=True)
+    allowsApiOverage = graphene.Boolean(required=True)
+    details = graphene.Field(UsageDetails, required=True)
 
 
 class CustomerSchema(graphene.ObjectType, UsageData):
